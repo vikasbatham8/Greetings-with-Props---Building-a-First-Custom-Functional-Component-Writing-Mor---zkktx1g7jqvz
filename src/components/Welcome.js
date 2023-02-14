b813b889113ae68f450cb6be8154b3b05e8fa552
Viikas Batham <112746619+vikasbatham8@users.noreply.github.com>
2023-02-14 21:07:48 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,13 @@
 // write code for Welcome component here
+import React from 'react';
+
+const Welcome = ({ name }) => {
+  return (
+    <div>
+      <h1>Hey {name}!</h1>
+      <h2>Welcome to Newton School.</h2>
+    </div>
+  );
+};
+
+export default Welcome;
